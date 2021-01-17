308f5f45adb846739561e3c6edc5b3f0af8d3302
officialsameel <55947656+officialsameel@users.noreply.github.com>
2021-01-17 20:05:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`UnRealMaow`, { type: "LISTENING" });
+  client.user.setActivity(`UnRealMaow#5900`, { type: "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
